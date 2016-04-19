6189abda6aae649664cef4a0cfbd6850936baffc
Izabela Cardoso <cizabelacristina@gmail.com>
2016-04-19 16:18:52 -0300

    Refactoring user interface

--- app/src/main/java/dao/PlaceDAO.java
+++ app/src/main/java/dao/PlaceDAO.java
@@ -1,11 +1,16 @@
 package dao;
 
+import android.support.v4.app.FragmentActivity;
+
 import org.json.JSONObject;
 
 public class PlaceDAO extends DAO{
 
     public PlaceDAO(){
         
+    }
+
+    public PlaceDAO(FragmentActivity activity){
     }
 
     public JSONObject searchPlaceByPartName(String name){
@@ -15,5 +20,15 @@ public class PlaceDAO extends DAO{
         JSONObject placeByPartNameQueryResult = this.executeConsult(QUERY);
 
         return placeByPartNameQueryResult;
+    }
+
+    public JSONObject searchAllPlaces()
+    {
+        return this.executeConsult("SELECT * FROM vw_place ORDER BY evaluate DESC");
+    }
+
+    public JSONObject searchTop5Places()
+    {
+        return this.executeConsult("SELECT * FROM vw_place ORDER BY evaluate DESC LIMIT 5");
     }
 }
