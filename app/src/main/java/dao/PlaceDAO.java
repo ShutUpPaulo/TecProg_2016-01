157e757d06db9d3bbabcc9fb4b967cbeab98d355
Izabela Cardoso <cizabelacristina@gmail.com>
2016-04-19 13:08:09 -0300

    Issue #15: Applying techniques in PlaceDAO.java

--- app/src/main/java/dao/PlaceDAO.java
+++ app/src/main/java/dao/PlaceDAO.java
@@ -1,32 +1,19 @@
 package dao;
 
-import android.app.Activity;
-
 import org.json.JSONObject;
 
-import model.Place;
+public class PlaceDAO extends DAO{
 
-/**
- * Created by viny on 30/09/15.
- */
-public class PlaceDAO extends DAO {
-
-    public PlaceDAO(Activity currentActivity) {
-        super(currentActivity);
+    public PlaceDAO(){
+        
     }
 
-    public PlaceDAO(){}
+    public JSONObject searchPlaceByPartName(String name){
 
-    public JSONObject searchPlaceByPartName(String name)
-    {
-        return this.executeConsult("SELECT * FROM vw_place WHERE namePlace LIKE '%" + name + "%'");
-    }
-    public JSONObject searchAllPlaces()
-    {
-        return this.executeConsult("SELECT * FROM vw_place ORDER BY evaluate DESC");
-    }
-    public JSONObject searchTop5Places()
-    {
-        return this.executeConsult("SELECT * FROM vw_place ORDER BY evaluate DESC LIMIT 5");
+        final String QUERY = "SELECT * FROM vw_place WHERE namePlace LIKE '%" + name + "%'";
+
+        JSONObject placeByPartNameQueryResult = this.executeConsult(QUERY);
+
+        return placeByPartNameQueryResult;
     }
 }
