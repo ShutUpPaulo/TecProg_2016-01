ef12da19ffa2034fd896cb7f82565087a6d9bc03
Izabela Cardoso <cizabelacristina@gmail.com>
2016-04-24 10:03:18 -0300

    Issue #13: Applying formatting rules in PlaceAdapter.java

--- app/src/main/java/com/mathheals/euvou/controller/showPlaceRanking/PlaceAdapter.java
+++ app/src/main/java/com/mathheals/euvou/controller/showPlaceRanking/PlaceAdapter.java
@@ -6,41 +6,46 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
-
 import com.mathheals.euvou.R;
-
 import java.util.List;
-
 import model.Place;
 
-/**
- * Created by geovanni on 16/11/15.
- */
-public class PlaceAdapter extends ArrayAdapter<Place> {
-    public PlaceAdapter(Context context, List<Place> places) {
+public class PlaceAdapter extends ArrayAdapter<Place>{
+
+    public PlaceAdapter(Context context, List<Place> places){
         super(context, 0,places);
     }
-    private static class ViewHolder {
+
+    private static class ViewHolder{
         TextView placeName;
         TextView placeEvaluation;
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+    public View getView(int position, View convertView, ViewGroup parent){
+
         Place place = getItem(position);
         ViewHolder viewHolder;
-        if (convertView == null) {
+
+        if(convertView == null){
             viewHolder = new ViewHolder();
-            convertView = LayoutInflater.from(getContext()).inflate(R.layout.fragment_show_place_rank, parent, false);
+
+            convertView = LayoutInflater.from(getContext())
+                                        .inflate(R.layout.fragment_show_place_rank, parent, false);
+
             viewHolder.placeName = (TextView) convertView.findViewById(R.id.placeName);
             viewHolder.placeEvaluation = (TextView) convertView.findViewById(R.id.placeEvaluation);
+
             convertView.setTag(viewHolder);
-        } else {
+        }
+        else{
             viewHolder = (ViewHolder) convertView.getTag();
         }
+
         viewHolder.placeName.setText(
                 ((place.getName().length() > 40) ? place.getName().substring(0, 39).concat("...") : place.getName()));
         viewHolder.placeEvaluation.setText(place.getEvaluate().toString());
+        
         return convertView;
     }
 }
