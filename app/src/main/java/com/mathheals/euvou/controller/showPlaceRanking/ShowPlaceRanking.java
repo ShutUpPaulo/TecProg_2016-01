52b3a39ebb004b7372c99b6687fe9f2e336c8b94
Izabela Cardoso <cizabelacristina@gmail.com>
2016-04-24 09:44:16 -0300

    Applying formatting rules in ShowPlaceRanking.java

--- app/src/main/java/com/mathheals/euvou/controller/showPlaceRanking/ShowPlaceRanking.java
+++ app/src/main/java/com/mathheals/euvou/controller/showPlaceRanking/ShowPlaceRanking.java
@@ -1,30 +1,26 @@
 package com.mathheals.euvou.controller.showPlaceRanking;
 
-import android.app.Activity;
-import android.net.Uri;
 import android.os.Bundle;
-import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import com.mathheals.euvou.R;
 
-public class ShowPlaceRanking extends android.support.v4.app.Fragment {
+public class ShowPlaceRanking extends android.support.v4.app.Fragment{
 
-    public ShowPlaceRanking() {
+    public ShowPlaceRanking(){
         // Required empty public constructor
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+                             Bundle savedInstanceState){
+
         return inflater.inflate(R.layout.fragment_show_place_ranking, container, false);
     }
 
