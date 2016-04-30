a2bd22df227b43af975f0ba41691b886ee9923bc
ShutUpPaulo <markes.calado@gmail.com>
2016-04-30 02:32:24 -0300

    Issue #14: Applying formatting techniques to ActivityUtility class.

--- app/src/main/java/com/mathheals/euvou/controller/utility/ActivityUtility.java
+++ app/src/main/java/com/mathheals/euvou/controller/utility/ActivityUtility.java
@@ -1,3 +1,8 @@
+/*
+ *File: ActivityUtility.java
+ * Purpose: allows the Back Stack to be cleared and restart the activity when is needed.
+ */
+
 package com.mathheals.euvou.controller.utility;
 
 import android.app.Activity;
@@ -5,22 +10,21 @@ import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 
-/**
- * Created by marlonmendes on 14/10/15.
- */
-public class ActivityUtility {
+public class ActivityUtility{
 
-    public static void restartActivity(Activity activity) {
+    public static void restartActivity(Activity activity){
         Intent intent = activity.getIntent();
         activity.finish();
         activity.startActivity(intent);
     }
 
-    public static void clearBackStack(FragmentActivity fragmentActivity) {
+    public static void clearBackStack(FragmentActivity fragmentActivity){
         FragmentManager manager = fragmentActivity.getSupportFragmentManager();
-        if (manager.getBackStackEntryCount() > 0) {
+        if (manager.getBackStackEntryCount() > 0){
             FragmentManager.BackStackEntry first = manager.getBackStackEntryAt(0);
             manager.popBackStack(first.getId(), FragmentManager.POP_BACK_STACK_INCLUSIVE);
+        }else{
+            //Nothing to do.
         }
     }
 }
