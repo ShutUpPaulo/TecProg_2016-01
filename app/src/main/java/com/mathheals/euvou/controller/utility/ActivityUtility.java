0ad819647f922c2c426978987d615afcf098fcb8
ShutUpPaulo <markes.calado@gmail.com>
2016-04-30 02:49:16 -0300

    Issue #26: Applying logs to class ActivityUtility.

--- app/src/main/java/com/mathheals/euvou/controller/utility/ActivityUtility.java
+++ app/src/main/java/com/mathheals/euvou/controller/utility/ActivityUtility.java
@@ -9,10 +9,16 @@ import android.app.Activity;
 import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
+import android.util.Log;
 
 public class ActivityUtility{
 
+    private static final String TAG = "ActivityLog";
+
     public static void restartActivity(Activity activity){
+
+        Log.d(TAG, "restartActivity: beggining the restart of the activity");
+
         Intent intent = activity.getIntent();
         activity.finish();
         activity.startActivity(intent);
@@ -21,6 +27,7 @@ public class ActivityUtility{
     public static void clearBackStack(FragmentActivity fragmentActivity){
         FragmentManager manager = fragmentActivity.getSupportFragmentManager();
         if (manager.getBackStackEntryCount() > 0){
+            Log.d(TAG, "clearBackStack: the back stack is ready to be cleared");
             FragmentManager.BackStackEntry first = manager.getBackStackEntryAt(0);
             manager.popBackStack(first.getId(), FragmentManager.POP_BACK_STACK_INCLUSIVE);
         }else{
