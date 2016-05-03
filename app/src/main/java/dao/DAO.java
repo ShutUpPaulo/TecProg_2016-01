9cdd9cb46df09b3a7443420c580053bb3ec62b7d
João Vitor Araujo <joao18araujo@gmail.com>
2016-05-03 20:55:49 -0300

    Issue #16: Applying asserts in DAO class

--- app/src/main/java/dao/DAO.java
+++ app/src/main/java/dao/DAO.java
@@ -9,10 +9,11 @@ import java.util.Calendar;
 
 public abstract class DAO {
 
-    private final int LIMIT_CONNECTION_TIME = 15000;
-    private final String URL_QUERY = "http://euvou.esy.es/query.php";
-    private final String URL_CONSULT = "http://euvou.esy.es/consult.php";
-    private final String CONNECTION_PROBLEM_MESSAGE =
+    private static final int LIMIT_CONNECTION_TIME = 15000;
+    private static final String STRING_EMPTY = "";
+    private static final String URL_QUERY = "http://euvou.esy.es/query.php";
+    private static final String URL_CONSULT = "http://euvou.esy.es/consult.php";
+    private static final String CONNECTION_PROBLEM_MESSAGE =
             "Problema de conexão com o servidor (verifique se esta conectado a internet)";
 
     private Activity currentActivity;
@@ -26,12 +27,17 @@ public abstract class DAO {
     }
 
     private String processQuery(String query,String urlQuery) {
+        assert query != null;
+        assert urlQuery != null;
+
         Consult consult = new Consult(query,urlQuery);
         consult.exec();
 
         long currentTime = Calendar.getInstance().getTime().getTime();
+        assert currentTime >= 0;
 
         long timeLimit = currentTime + LIMIT_CONNECTION_TIME;
+        assert timeLimit >= 0;
 
         while(!consult.getIsDoing() && currentTime < timeLimit) {
             currentTime = Calendar.getInstance().getTime().getTime();
@@ -47,27 +53,40 @@ public abstract class DAO {
         return consult.getResult();
     }
     public static boolean limitExceeded(long timeLimit, long currentTime){
-        boolean isLimitExceeded = false;
+        assert timeLimit >= 0;
+        assert currentTime >= 0;
+
+        boolean isLimitExceeded;
 
         if(currentTime >= timeLimit) {
             isLimitExceeded = true;
         }else{
-            //nothing to do
+            isLimitExceeded = false;
         }
 
         return isLimitExceeded;
     }
 
     protected String executeQuery(String query){
+        assert query != null;
+
         String executedQuery = processQuery(query, URL_QUERY);
+        assert executedQuery != null;
+
         return executedQuery;
     }
 
     protected JSONObject executeConsult(String query) {
-        String consultJson;
+        assert query != null;
+
+        String consultJson = STRING_EMPTY;
+
         JSONObject jsonObject = null;
+
         try {
             consultJson = processQuery(query, URL_CONSULT);
+            assert consultJson != null;
+
             jsonObject  = new JSONObject(consultJson);
         } catch (Exception e) {
             e.printStackTrace();
