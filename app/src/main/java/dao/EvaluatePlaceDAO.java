f3079d3f5c1df2ef430cfe4a6f4865f16412e754
João Vitor Araujo <joao18araujo@gmail.com>
2016-05-05 21:40:37 -0300

    Issue #14: Applying formatting techniques in EvaluatePlaceDAO class

--- app/src/main/java/dao/EvaluatePlaceDAO.java
+++ app/src/main/java/dao/EvaluatePlaceDAO.java
@@ -11,6 +11,8 @@ import model.Evaluation;
  */
 public class EvaluatePlaceDAO extends DAO{
 
+    private static final String STRING_EMPTY = "";
+
     public EvaluatePlaceDAO() {
 
     }
@@ -20,18 +22,22 @@ public class EvaluatePlaceDAO extends DAO{
     }
 
     public void evaluatePlace(Evaluation evaluation) {
-        final String evaluationQuery;
+        String evaluationQuery = STRING_EMPTY;
 
-        JSONObject findEvaluation = executeConsult("SELECT * FROM evaluate_place WHERE idPlace = \"" + evaluation.getIdPlace() + "\" " +
-                "AND idUser = \"" + evaluation.getIdUser() + "\"");
+        String consultEvaluation = "SELECT * FROM evaluate_place WHERE idPlace = \"" +
+                evaluation.getIdPlace() + "\" " + "AND idUser = \"" +
+                evaluation.getIdUser() + "\"";
+
+        JSONObject findEvaluation = executeConsult(consultEvaluation);
 
         if(findEvaluation==null) {
-            evaluationQuery = "INSERT INTO evaluate_place(grade, idUser, idPlace) VALUES (\"" + evaluation.getgrade() + "\"," +
-                    "\"" + evaluation.getIdUser() + "\"," +
+            evaluationQuery = "INSERT INTO evaluate_place(grade, idUser, idPlace) VALUES (\"" +
+                    evaluation.getgrade() + "\"," + "\"" + evaluation.getIdUser() + "\"," +
                     "\"" + evaluation.getIdPlace() + "\")";
         }else{
-            evaluationQuery = "UPDATE evaluate_place SET grade = \"" +evaluation.getgrade() + "\" " +
-                    "WHERE idPlace = \"" + evaluation.getIdPlace() + "\" AND idUser = \"" + evaluation.getIdUser() + "\"";
+            evaluationQuery = "UPDATE evaluate_place SET grade = \"" +
+                    evaluation.getgrade() + "\" " + "WHERE idPlace = \"" + evaluation.getIdPlace() +
+                    "\" AND idUser = \"" + evaluation.getIdUser() + "\"";
         }
 
         executeQuery(evaluationQuery);
@@ -39,8 +45,8 @@ public class EvaluatePlaceDAO extends DAO{
 
 
     public JSONObject searchPlaceEvaluation(int placeId, int userId) {
-        String searchPlaceEvaluationQuery = "SELECT * FROM evaluate_place WHERE idUser = \"" + userId
-                           + "\" AND idPlace = " + placeId;
+        String searchPlaceEvaluationQuery = "SELECT * FROM evaluate_place WHERE idUser = \"" +
+                userId + "\" AND idPlace = " + placeId;
         return executeConsult(searchPlaceEvaluationQuery);
     }
 }
