bad58e8e776a36906152da5be6827b454d8000cc
João Vitor Araujo <joao18araujo@gmail.com>
2016-05-08 13:58:17 -0300

    Issue #29: Creating javadoc comments in RegisterFragment class

--- app/src/main/java/com/mathheals/euvou/controller/user_registration/RegisterFragment.java
+++ app/src/main/java/com/mathheals/euvou/controller/user_registration/RegisterFragment.java
@@ -49,10 +49,20 @@ public class RegisterFragment extends Fragment implements View.OnClickListener{
     private String passwordConfirmation = STRING_EMPTY;
     private String mailConfirmation = STRING_EMPTY;
 
-
+    /**
+     * Empty constructor required in RegisterFragment tests
+     */
     public RegisterFragment(){
     }
 
+    /**
+     * Creates and returns the view hierarchy associated with the fragment
+     * @param inflater - Object used to inflate any views in the fragment
+     * @param container - If non-null, is the parent view that the fragment should be attached to
+     * @param savedInstanceState - If non-null, this fragment is being re-constructed from a
+     *                             previous saved state as given here
+     * @return View - View of the Register fragment
+     */
     @Override
     public View onCreateView(LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState){
@@ -76,6 +86,10 @@ public class RegisterFragment extends Fragment implements View.OnClickListener{
         return view;
     }
 
+    /**
+     * Registers new user in database
+     * @param user - Object that contain information of new user
+     */
     private void registerUser(User user){
         assert user != null;
 
@@ -84,6 +98,9 @@ public class RegisterFragment extends Fragment implements View.OnClickListener{
         userDAO.saveUser(user);
     }
 
+    /**
+     * Starts login activity
+     */
     private void startLoginActivity(){
         Activity activity = getActivity();
         assert activity != null;
@@ -93,6 +110,10 @@ public class RegisterFragment extends Fragment implements View.OnClickListener{
         activity.startActivity(loginIntent);
     }
 
+    /**
+     * Sets conditions to edit new user information
+     * @param view - current view where the texts will be edited
+     */
     private void settingEditText(View view){
         this.nameField = (EditText) view.findViewById(R.id.nameField);
         assert this.nameField != null;
@@ -119,6 +140,9 @@ public class RegisterFragment extends Fragment implements View.OnClickListener{
         assert this.birthDateField != null;
     }
 
+    /**
+     * Sets the previous typed text of the new user
+     */
     private void settingTextTyped(){
         this.name = nameField.getText().toString();
         this.username = usernameField.getText().toString();
@@ -129,12 +153,23 @@ public class RegisterFragment extends Fragment implements View.OnClickListener{
         this.birthDate = birthDateField.getText().toString();
     }
 
+    /**
+     * Sets the information typed on registration view
+     * @param view - current view where the text was edited
+     */
     @Override
     public void onClick(View view){
         assert view != null;
 
         this.settingTextTyped();
 
+        this.validateUserInformation();
+    }
+
+    /**
+     * Validates user information and registers if is valid
+     */
+    private void validateUserInformation(){
         try {
             User user = new User(name, username, mail, mailConfirmation,
                     password, passwordConfirmation, birthDate);
