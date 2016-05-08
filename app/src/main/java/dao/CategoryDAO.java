235ff7d33f796595b29d1ebd8bd916240ac9c654
João Vitor Araujo <joao18araujo@gmail.com>
2016-05-08 19:53:29 -0300

    Issue #29: Creating javadoc comments in CategoryDAO class

--- app/src/main/java/dao/CategoryDAO.java
+++ app/src/main/java/dao/CategoryDAO.java
@@ -9,12 +9,21 @@ import android.app.Activity;
 
 import org.json.JSONObject;
 
-public class CategoryDAO extends DAO {
+public class CategoryDAO extends DAO{
 
+    /**
+     * Constructs based on current activity
+     * @param currentActivity Activity used on construction
+     */
     public CategoryDAO(Activity currentActivity){
         super(currentActivity);
     }
 
+    /**
+     * Searches category by Id
+     * @param idCategory Id of searched category
+     * @return JSONObject based on executed database consult
+     */
     public JSONObject searchCategoryById(int idCategory){
         String consultQueryString =
                 "SELECT nameCategory FROM tb_category WHERE idCategory = " + idCategory;
