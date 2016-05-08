7193167736fe148ae1410f6da0d31e5bf11ce666
João Vitor Araujo <joao18araujo@gmail.com>
2016-05-08 19:00:23 -0300

    Issue #29: Creating javadoc comments in RemoveUserVIewMessages class

--- app/src/main/java/com/mathheals/euvou/controller/remove_user/RemoveUserVIewMessages.java
+++ app/src/main/java/com/mathheals/euvou/controller/remove_user/RemoveUserVIewMessages.java
@@ -9,15 +9,23 @@ package com.mathheals.euvou.controller.remove_user;
 import android.content.Context;
 import android.widget.Toast;
 
-public class RemoveUserVIewMessages {
+public class RemoveUserVIewMessages{
     private static final String BYE_BYE_MESSAGE = "Conta desativada :(";
     private static final String WELCOME_BACK_MESSAGE = "Seja bem vindo novamente!";
 
-
-    public static void showWelcomeBackMessage(Context context) {
+    /**
+     * Shows "Welcome Back" message
+     * @param context Context needed in makeTest method of Toast class
+     */
+    public static void showWelcomeBackMessage(Context context){
         Toast.makeText(context, WELCOME_BACK_MESSAGE, Toast.LENGTH_LONG).show();
     }
-    public static void showAccountDeactivateMessage(Context context) {
+
+    /**
+     * Shows "Account Deactivate" message
+     * @param context Context needed in makeTest method of Toast class
+     */
+    public static void showAccountDeactivateMessage(Context context){
         Toast.makeText(context, BYE_BYE_MESSAGE, Toast.LENGTH_LONG).show();
     }
 }
