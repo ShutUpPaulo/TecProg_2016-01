85549c06be0a6629097ed4165a249e2d53bfd08e
Izabela Cardoso <cizabelacristina@gmail.com>
2016-05-09 09:32:53 -0300

    Issue #27: Uncomplicate ShowTop5Ranking.java

--- app/src/main/java/com/mathheals/euvou/controller/showPlaceRanking/ShowTop5Ranking.java
+++ app/src/main/java/com/mathheals/euvou/controller/showPlaceRanking/ShowTop5Ranking.java
@@ -19,7 +19,6 @@ import android.widget.Toast;
 import com.mathheals.euvou.R;
 import com.mathheals.euvou.controller.event_recommendation.RecommendEvent;
 import com.mathheals.euvou.controller.show_place.ShowPlaceInfo;
-import com.mathheals.euvou.controller.utility.LoginUtility;
 import org.json.JSONException;
 import org.json.JSONObject;
 import java.text.ParseException;
@@ -31,91 +30,109 @@ import model.Place;
 public class ShowTop5Ranking extends android.support.v4.app.Fragment implements AdapterView.OnItemClickListener,
         View.OnClickListener{
 
-    private ListView listView;
     private ArrayList<Place> places;
 
-    public ShowTop5Ranking() {
-        // Required empty public constructor
+    public ShowTop5Ranking(){
+
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View vw = inflater.inflate(R.layout.fragment_show_top5_rank, container, false);
+                             Bundle savedInstanceState){
 
+        View top5RankingView = inflater.inflate(R.layout.fragment_show_top5_rank, container, false);
+
+        addRecommendedEventsFragment();
+
+        Button completeRankingButton = (Button) top5RankingView.findViewById(R.id.more);
+        completeRankingButton.setOnClickListener(this);
+
+        ListView listView = (ListView) top5RankingView.findViewById(R.id.listViewPlaces5);
+        listView.setOnItemClickListener(this);
+
+        fillList(listView);
+
+        return  top5RankingView;
+    }
+
+    private void addRecommendedEventsFragment(){
         Fragment recommendEventFragment = new RecommendEvent();
         FragmentTransaction fragmentTransaction = getChildFragmentManager().beginTransaction();
         fragmentTransaction.add(R.id.recommendedEventList, recommendEventFragment).commit();
-
-        Button completeRankingButton = (Button) vw.findViewById(R.id.more);
-        completeRankingButton.setOnClickListener(this);
-
-        // Inflate the layout for this fragment
-        listView = (ListView) vw.findViewById(R.id.listViewPlaces5);
-        listView.setOnItemClickListener(this);
-        fillList();
-        return  vw;
     }
 
-    private void fillList() {
-        try {
-            int id = (new LoginUtility(getActivity())).getUserId();
-            JSONObject result = new PlaceDAO(getActivity()).searchTop5Places();
-            //List<Map<String, String>> placeList= new ArrayList<Map<String, String>>();
-            places = new ArrayList<>();
-            for (int i = 0; i < result.length(); i++) {
-                int idPlace = result.getJSONObject("" + i).getInt("idPlace");
-                String namePlace = result.getJSONObject("" + i).getString("namePlace");
-                Place aux = new Place(idPlace,
-                        namePlace,
-                        result.getJSONObject("" + i).getString("evaluate"),
-                        result.getJSONObject("" + i).getString("longitude"),
-                        result.getJSONObject("" + i).getString("latitude"),
-                        result.getJSONObject("" + i).getString("operation"),
-                        result.getJSONObject("" + i).getString("description"),
-                        result.getJSONObject("" + i).getString("address"),
-                        result.getJSONObject("" + i).getString("phonePlace")
-                );
+    private void fillList(ListView listToBeFilled){
+        assert listToBeFilled != null;
+
+        JSONObject placesData = new PlaceDAO(getActivity()).searchTop5Places();
+        places = new ArrayList<Place>();
+        populateArrayOfPlaces(placesData,places);
+
+        //Sets the required adapter to adapt items of the places array in items of the places list
+        PlaceAdapter placeAdapter = new PlaceAdapter(getActivity(),places);
+
+        assert placeAdapter != null;
+
+        listToBeFilled.setAdapter(placeAdapter);
+
+        assert listToBeFilled.getAdapter() == placeAdapter;
+    }
+
+    private void populateArrayOfPlaces(JSONObject placesData, ArrayList<Place> places){
+        try{
+            int placesDataSize = placesData.length();
+
+            for(int i = 0; i < placesDataSize; i++){
+
+                int idPlace = placesData.getJSONObject("" + i).getInt("idPlace");
+                String placeName = placesData.getJSONObject("" + i).getString("namePlace");
+                String placeEvaluate = placesData.getJSONObject("" + i).getString("evaluate");
+                String placeLongitude = placesData.getJSONObject("" + i).getString("longitude");
+                String placeLatitude = placesData.getJSONObject("" + i).getString("latitude");
+                String placeOperationTime = placesData.getJSONObject("" + i).getString("operation");
+                String placeDescription = placesData.getJSONObject("" + i).getString("description");
+                String placeAddress = placesData.getJSONObject("" + i).getString("address");
+                String placePhone = placesData.getJSONObject("" + i).getString("phonePlace");
+
+                Place aux = new Place(idPlace, placeName, placeEvaluate, placeLongitude,
+                        placeLatitude, placeOperationTime, placeDescription, placeAddress,
+                        placePhone);
+
+                int placesArrayListSize = places.size();
+
                 places.add(aux);
-                //placeList.add(placeRank(place.getJSONObject("" + i).getString("namePlace"), place.getJSONObject("" + i).getString("evaluate")));
+
+                assert places.size() == placesArrayListSize + 1;
+                assert places.get(placesArrayListSize) == aux;
 
             }
-            PlaceAdapter placeAdapter = new PlaceAdapter(getActivity(),places);
-            /*SimpleAdapter simpleAdapter = new SimpleAdapter(getActivity(),placeList,
-                    android.R.layout.simple_list_item_2,
-                    new String[]{"Nome","Nota"}, new int[]{android.R.id.text1});
-
-            Toast.makeText(getActivity(), simpleAdapter.toString(), Toast.LENGTH_SHORT).show();
-*/
-            listView.setAdapter(placeAdapter);
-
-        } catch (JSONException e) {
+        }catch (JSONException e){
             e.printStackTrace();
-        } catch (PlaceException e) {
+        } catch (PlaceException e){
             e.printStackTrace();
-        } catch (ParseException e) {
+        } catch (ParseException e){
             e.printStackTrace();
         }
     }
+
     @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id){
         startShowInfoActivity(position);
 
     }
 
-    private void startShowInfoActivity(int id) {
+    private void startShowInfoActivity(int id){
         Intent intent = new Intent(getActivity(), ShowPlaceInfo.class);
         intent.putExtras(getPlaceInfoAsBundle(id));
         startActivity(intent);
     }
 
-    private Bundle getPlaceInfoAsBundle(int id) {
+    private Bundle getPlaceInfoAsBundle(int id){
         Bundle placeInfo = new Bundle();
         Toast.makeText(getActivity(), "" + id, Toast.LENGTH_LONG);
         placeInfo.putString("name", places.get(id).getPlaceName());
@@ -130,7 +147,7 @@ public class ShowTop5Ranking extends android.support.v4.app.Fragment implements 
     }
 
     @Override
-    public void onClick(View v) {
+    public void onClick(View v){
         android.support.v4.app.FragmentTransaction fragmentTransaction = getActivity().getSupportFragmentManager().beginTransaction();
         fragmentTransaction.replace(R.id.content_frame, new ShowPlaceRank());
         fragmentTransaction.addToBackStack(null);
