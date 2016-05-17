321b472953b81355da9d23d095ea3fb5d9eb4e22
ShutUpPaulo <markes.calado@gmail.com>
2016-05-17 15:16:51 -0300

    Issue #29 - Creating javado to Evaluation class.

--- app/src/main/java/model/Evaluation.java
+++ app/src/main/java/model/Evaluation.java
@@ -10,32 +10,62 @@ public class Evaluation{
     private int idPlace;
     private int idUser;
 
+    /**
+     * Constructs the evaluation with the given information
+     * @param idPlace - The ID of the place where the event will happen
+     * @param idUser -  The ID of the user
+     * @param grid - The grid of the event
+     */
     public Evaluation(int idPlace, int idUser, float grid){
         setIdPlace(idPlace);
         setIdUser(idUser);
         setGrid(grid);
     }
 
+    /**
+     * Sets the grid of the event
+     * @param grid
+     */
     private void setGrid(float grid){
         this.grid = grid;
     }
 
+    /**
+     * Sets the ID of the user
+     * @param idUser
+     */
     private  void setIdUser(int idUser){
         this.idUser = idUser;
     }
 
+    /**
+     * Sets the ID of the place that will be evaluated
+     * @param idPlace
+     */
     private void setIdPlace(int idPlace){
         this.idPlace = idPlace;
     }
 
+    /**
+     * Gets the grid
+     * @return float - grid
+     */
     public float getGrid(){
         return grid;
     }
 
+    /**
+     * Gets the ID of the place
+     * @return int - The place ID
+     */
     public int getIdPlace(){
         return idPlace;
     }
 
+    /**
+     * Gets the ID of the user
+     * @return int - The user ID
+     */
     public int getIdUser(){
         return idUser;
     }
