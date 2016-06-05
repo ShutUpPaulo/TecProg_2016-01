26f5337f4d3515e4a296a7a2c0596d46aa5c88f6
Igor Ribeiro Barbosa Duarte <igor.ribeiro.duarte@gmail.com>
2016-06-04 23:00:49 -0300

    Issue #27: Uncomplicating DisableAccountFragment class

--- app/src/main/java/com/mathheals/euvou/controller/remove_user/DisableAccountFragment.java
+++ app/src/main/java/com/mathheals/euvou/controller/remove_user/DisableAccountFragment.java
@@ -22,12 +22,13 @@ import com.mathheals.euvou.R;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class DisableAccountFragment extends android.support.v4.app.Fragment implements View.OnClickListener {
+public class DisableAccountFragment extends android.support.v4.app.Fragment
+        implements View.OnClickListener{
 
     /**
      * Required constructor to instantiate a fragment object
      */
-    public DisableAccountFragment() {
+    public DisableAccountFragment(){
 
     }
 
@@ -41,7 +42,7 @@ public class DisableAccountFragment extends android.support.v4.app.Fragment impl
      */
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+                             Bundle savedInstanceState){
         // Inflate the layout for this fragment
 
         assert inflater != null;
@@ -50,11 +51,7 @@ public class DisableAccountFragment extends android.support.v4.app.Fragment impl
 
         View view = inflater.inflate(R.layout.fragment_disable_account, container, false);
 
-        Button yesButton = (Button)view.findViewById(R.id.button_yes_id);
-        yesButton.setOnClickListener(this);
-
-        Button noButton = (Button)view.findViewById(R.id.button_no_id);
-        noButton.setOnClickListener(this);
+        setButtons(view);
 
         return view;
     }
@@ -62,28 +59,71 @@ public class DisableAccountFragment extends android.support.v4.app.Fragment impl
     /**
      * Disable the account when the user clicks on the yes button or comes back to the previous
      * screen if the user clicks on the no button
-     * @param view Current view where the text was edited
+     * @param view - Current view where the text was edited
      */
     @Override
-    public void onClick(View view) {
+    public void onClick(View view){
         assert view != null;
 
         FragmentActivity activity = this.getActivity();
-        FragmentTransaction fragmentTransaction = activity.getSupportFragmentManager().beginTransaction();
-        FragmentManager fragmentManager = activity.getSupportFragmentManager();
-        Context homePageContext = activity.getBaseContext();
 
         switch(view.getId()) {
             case R.id.button_yes_id:
-                fragmentManager.popBackStack();
-                RemoveUserVIewMessages.showWelcomeBackMessage(homePageContext);
-                return;
+                showWelcomeBackMessage(activity);
+                break;
             case R.id.button_no_id:
-                android.support.v4.app.Fragment disableAccountFragment = activity.getSupportFragmentManager().findFragmentByTag(String.valueOf(R.string.DISABLE_ACCOUNT_FRAGMENT_TAG));
-                fragmentTransaction.remove(disableAccountFragment);
-                fragmentTransaction.add(R.id.content_frame, new DisableAccountLoginConfirmation());
-                fragmentTransaction.addToBackStack(null);
-                fragmentTransaction.commit();
+                callsDisableAccountLoginConfirmation(activity);
+                break;
         }
+    }
+
+    /**
+     * Set the buttons on view
+     * @param view - Current view
+     */
+    private void setButtons(View view){
+        assert view != null;
+
+        Button yesButton = (Button)view.findViewById(R.id.button_yes_id);
+        yesButton.setOnClickListener(this);
+
+        Button noButton = (Button)view.findViewById(R.id.button_no_id);
+        noButton.setOnClickListener(this);
+    }
+
+    /**
+     * Displays the Disable Account Confirmation Page
+     * @param activity - Base class for activity that want to use the support-based Fragment and
+     *                 Loader APIs.
+     */
+    private void callsDisableAccountLoginConfirmation(FragmentActivity activity){
+        assert activity != null;
+
+        FragmentTransaction fragmentTransaction =
+                activity.getSupportFragmentManager().beginTransaction();
+
+        android.support.v4.app.Fragment disableAccountFragment =
+                activity.getSupportFragmentManager().findFragmentByTag(
+                        String.valueOf(R.string.DISABLE_ACCOUNT_FRAGMENT_TAG));
+        
+        fragmentTransaction.remove(disableAccountFragment);
+        fragmentTransaction.add(R.id.content_frame, new DisableAccountLoginConfirmation());
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+    }
+
+    /**
+     * Displays a page with a welcome back message
+     * @param activity - Base class for activity that want to use the support-based Fragment and
+     *                 Loader APIs.
+     */
+    private void showWelcomeBackMessage(FragmentActivity activity){
+        assert activity != null;
+
+        FragmentManager fragmentManager = activity.getSupportFragmentManager();
+        Context homePageContext = activity.getBaseContext();
+
+        fragmentManager.popBackStack();
+        RemoveUserVIewMessages.showWelcomeBackMessage(homePageContext);
     }
 }
